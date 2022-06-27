63f1298c996d6486d72bf0ee0bfae4ae7c52e9dd
ToSn0w <71845716+ToSn0w@users.noreply.github.com>
2022-06-27 14:16:38 +0200

    Create Productivity_20220627141615.js

---
+++ .history/components/Productivity/Productivity_20220627141615.js
@@ -0,0 +1,82 @@
+import React, { useEffect } from "react";
+
+import {
+  ProductivityWrapper,
+  ProdContainer,
+  ProdComponent,
+  ImageWrapper,
+  TextWrapper,
+  MainText,
+  SideText,
+  TextComponent,
+  ButtonComponent,
+} from "./styled/Productivity.elem";
+
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+import Image from "next/image";
+import MainButton from "../MainButton/MainButton";
+
+import { mainVariant, textVariant, childVariant } from "../Animations/Variants";
+
+const Productivity = () => {
+  //animations
+  const animation = useAnimation();
+  //intersection-observer
+  const [functionRef, inView] = useInView({
+    triggerOnce: true,
+    rootMargin: "-400px",
+  });
+
+  useEffect(() => {
+    if (inView) {
+      animation.start("animate");
+    }
+  }, [animation, inView]);
+  return (
+    <ProductivityWrapper>
+      <motion.div
+        ref={functionRef}
+        animate={animation}
+        initial="initial"
+        variants={mainVariant}
+      >
+        <ProdContainer>
+          <ProdComponent>
+            <ImageWrapper>
+              <Image
+                src="/images/Productivity/1.jpg"
+                width={746}
+                height={480}
+                alt="productivity1"
+                layout="responsive"
+              />
+            </ImageWrapper>
+          </ProdComponent>
+          <ProdComponent>
+            <TextComponent>
+              <TextWrapper>
+                <MainText
+                  variants={textVariant}
+                  initial="initial"
+                  animate="animate"
+                >
+                  POZNAJ WIĘCEJ ROZWIĄZAŃ W ZAKRESIE PRODUKTYWNOŚCI
+                </MainText>
+                <SideText variants={childVariant}>
+                  Zobacz, jak jeszcze Quest może pomóc Ci pracować efektywniej.
+                </SideText>
+                <ButtonComponent variants={childVariant}>
+                  <MainButton text={"więcej informacji"} arrow greyBorder />
+                </ButtonComponent>
+              </TextWrapper>
+            </TextComponent>
+          </ProdComponent>
+        </ProdContainer>
+      </motion.div>
+    </ProductivityWrapper>
+  );
+};
+
+export default Productivity;
